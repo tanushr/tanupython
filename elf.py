81387a17dfc29e992ce0e2b9c41f2f47667cd6ef
tanushr <43023087+tanushr@users.noreply.github.com>
2018-09-20 09:03:19 +0530

    Add files via upload

---
+++ elf.py
@@ -0,0 +1,13 @@
+x=int(input("enter your percentage"))
+if(x>=90):
+	print('A')
+elif(x>=80):
+	print('B')
+elif(x>=70):
+	print('C')
+elif(x>=60):
+	print('D')
+elif(x>=40):
+	print('E')
+else:
+	print("fail")
