0b82e1e15635823c0b968f02f4f921d87666c6e2
tanushr <43023087+tanushr@users.noreply.github.com>
2018-09-20 10:12:36 +0530

    Add files via upload

---
+++ rand1.py
@@ -0,0 +1,15 @@
+dl205@soetcse:~/tanu123$ python rand.py
+1
+dl205@soetcse:~/tanu123$ python rand.py
+1
+dl205@soetcse:~/tanu123$ python rand.py
+5
+dl205@soetcse:~/tanu123$ python rand.py
+3
+dl205@soetcse:~/tanu123$ python rand.py
+3
+dl205@soetcse:~/tanu123$ python rand.py
+4
+dl205@soetcse:~/tanu123$ python rand.py
+6
+
