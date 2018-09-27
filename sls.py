929dac6c1f0bc343e86fa16f4b9fa6bf9102acb0
tanushr <43023087+tanushr@users.noreply.github.com>
2018-09-27 09:07:31 +0530

    Add files via upload

---
+++ sls.py
@@ -0,0 +1,273 @@
+Enter number of players: 3
+
+----Player 1 Hit enter to roll----
+You rolled: 5
+You are at spot 6
+You climbed a Ladder to spot 17
+
+----Player 2 Hit enter to roll----
+You rolled: 5
+You are at spot 6
+You climbed a Ladder to spot 17
+
+----Player 3 Hit enter to roll----
+You rolled: 4
+You are at spot 5
+
+----Player 1 Hit enter to roll----
+You rolled: 4
+You are at spot 21
+
+----Player 2 Hit enter to roll----
+You rolled: 1
+You are at spot 18
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 11
+
+----Player 1 Hit enter to roll----
+You rolled: 2
+You are at spot 23
+
+----Player 2 Hit enter to roll----
+You rolled: 6
+You are at spot 24
+You climbed a Ladder to spot 26
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 17
+
+----Player 1 Hit enter to roll----
+You rolled: 5
+You are at spot 28
+
+----Player 2 Hit enter to roll----
+You rolled: 1
+You are at spot 27
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 23
+
+----Player 1 Hit enter to roll----
+You rolled: 6
+You are at spot 34
+
+----Player 2 Hit enter to roll----
+You rolled: 4
+You are at spot 31
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 29
+
+----Player 1 Hit enter to roll----
+You rolled: 3
+You are at spot 37
+
+----Player 2 Hit enter to roll----
+You rolled: 2
+You are at spot 33
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 35
+
+----Player 1 Hit enter to roll----
+You rolled: 5
+You are at spot 42
+
+----Player 2 Hit enter to roll----
+You rolled: 2
+You are at spot 35
+
+----Player 3 Hit enter to roll----
+You rolled: 5
+You are at spot 40
+
+----Player 1 Hit enter to roll----
+You rolled: 2
+You are at spot 44
+
+----Player 2 Hit enter to roll----
+You rolled: 3
+You are at spot 38
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 46
+
+----Player 1 Hit enter to roll----
+You rolled: 3
+You are at spot 47
+
+----Player 2 Hit enter to roll----
+You rolled: 3
+You are at spot 41
+
+----Player 3 Hit enter to roll----
+You rolled: 5
+You are at spot 51
+
+----Player 1 Hit enter to roll----
+You rolled: 6
+You are at spot 53
+
+----Player 2 Hit enter to roll----
+You rolled: 2
+You are at spot 43
+
+----Player 3 Hit enter to roll----
+You rolled: 3
+You are at spot 54
+
+----Player 1 Hit enter to roll----
+You rolled: 4
+You are at spot 57
+
+----Player 2 Hit enter to roll----
+You rolled: 2
+You are at spot 45
+
+----Player 3 Hit enter to roll----
+You rolled: 3
+You are at spot 57
+
+----Player 1 Hit enter to roll----
+You rolled: 6
+You are at spot 63
+
+----Player 2 Hit enter to roll----
+You rolled: 1
+You are at spot 46
+
+----Player 3 Hit enter to roll----
+You rolled: 1
+You are at spot 58
+
+----Player 1 Hit enter to roll----
+You rolled: 2
+You are at spot 65
+
+----Player 2 Hit enter to roll----
+You rolled: 5
+You are at spot 51
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 64
+
+----Player 1 Hit enter to roll----
+You rolled: 3
+You are at spot 68
+
+----Player 2 Hit enter to roll----
+You rolled: 4
+You are at spot 55
+
+----Player 3 Hit enter to roll----
+You rolled: 2
+You are at spot 66
+You rode a Snake to spot 53
+
+----Player 1 Hit enter to roll----
+You rolled: 2
+You are at spot 70
+
+----Player 2 Hit enter to roll----
+You rolled: 5
+You are at spot 60
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 59
+
+----Player 1 Hit enter to roll----
+You rolled: 2
+You are at spot 72
+
+----Player 2 Hit enter to roll----
+You rolled: 4
+You are at spot 64
+
+----Player 3 Hit enter to roll----
+You rolled: 2
+You are at spot 61
+
+----Player 1 Hit enter to roll----
+You rolled: 5
+You are at spot 77
+
+----Player 2 Hit enter to roll----
+You rolled: 2
+You are at spot 66
+You rode a Snake to spot 53
+
+----Player 3 Hit enter to roll----
+You rolled: 2
+You are at spot 63
+
+----Player 1 Hit enter to roll----
+You rolled: 3
+You are at spot 80
+
+----Player 2 Hit enter to roll----
+You rolled: 2
+You are at spot 55
+
+----Player 3 Hit enter to roll----
+You rolled: 5
+You are at spot 68
+
+----Player 1 Hit enter to roll----
+You rolled: 3
+You are at spot 83
+
+----Player 2 Hit enter to roll----
+You rolled: 4
+You are at spot 59
+
+----Player 3 Hit enter to roll----
+You rolled: 5
+You are at spot 73
+
+----Player 1 Hit enter to roll----
+You rolled: 4
+You are at spot 87
+
+----Player 2 Hit enter to roll----
+You rolled: 5
+You are at spot 64
+
+----Player 3 Hit enter to roll----
+You rolled: 1
+You are at spot 74
+
+----Player 1 Hit enter to roll----
+You rolled: 2
+You are at spot 89
+
+----Player 2 Hit enter to roll----
+You rolled: 3
+You are at spot 67
+
+----Player 3 Hit enter to roll----
+You rolled: 4
+You are at spot 78
+
+----Player 1 Hit enter to roll----
+You rolled: 1
+You are at spot 90
+
+----Player 2 Hit enter to roll----
+You rolled: 1
+You are at spot 68
+
+----Player 3 Hit enter to roll----
+You rolled: 6
+You are at spot 84
+You rode a Snake to spot 71
+Player 1 is the Winner!
+
