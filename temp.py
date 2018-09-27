60f8e37df2b0aace9dab355874946c5f306d0aa7
tanushr <43023087+tanushr@users.noreply.github.com>
2018-09-27 09:08:34 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,11 @@
+while True:
+	a=input("enter some value")
+	if(a=="t"):
+		import random
+		r=random.randint(25,30)
+		print("bangalore temperature is", r)
+	#	print(r)
+	else:
+		print("bye")
+		break	
+
