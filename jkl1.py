8b5a3474add333a617165aa9a40fe098ebb5fd12
tanushr <43023087+tanushr@users.noreply.github.com>
2018-10-04 10:15:02 +0530

    Add files via upload

---
+++ jkl1.py
@@ -0,0 +1,15 @@
+welcome to rock paper scissor
+3 = scissor
+enter 'k' for rock,'a' for paper,'s' for scissork
+p wins
+dl205@soetcse:~/tanu123$ python3 jkl.py
+welcome to rock paper scissor
+1 = rock
+enter 'k' for rock,'a' for paper,'s' for scissora
+p wins
+dl205@soetcse:~/tanu123$ python3 jkl.py
+welcome to rock paper scissor
+3 = scissor
+enter 'k' for rock,'a' for paper,'s' for scissors
+player wins
+
