11e9d20368a2b561e8a953bb8566e0e70e5b4049
tanushr <43023087+tanushr@users.noreply.github.com>
2018-10-04 10:15:24 +0530

    Add files via upload

---
+++ jjj1.py
@@ -0,0 +1,208 @@
+welcome to snake and ladder
+enter r to roll the dicer
+dice value is  6
+score is 6
+try again
+enter r to roll the dicer
+dice value is  1
+score is 7
+try again
+enter r to roll the dicer
+dice value is  6
+score is 13
+ climbed ladder
+enter r to roll the dicer
+dice value is  4
+score is 38
+snake bite
+enter r to roll the dicer 
+dice value is  4
+score is 13
+ climbed ladder
+enter r to roll the dicer
+dice value is  4
+score is 38
+snake bite
+enter r to roll the dicer
+dice value is  3
+score is 12
+try again
+enter r to roll the dicer
+dice value is  2
+score is 14
+try again
+enter r to roll the dicer
+dice value is  6
+score is 20
+try again
+enter r to roll the dicer
+dice value is  6
+score is 26
+try again
+enter r to roll the dicer
+dice value is  6
+score is 32
+try again
+enter r to roll the dicer
+dice value is  2
+score is 34
+try again
+enter r to roll the dicer
+dice value is  3
+score is 37
+try again
+enter r to roll the dicer
+dice value is  1
+score is 38
+snake bite
+enter r to roll the dicer
+dice value is  3
+score is 12
+try again
+enter r to roll the dicer
+dice value is  3
+score is 15
+try again
+enter r to roll the dicer
+dice value is  2
+score is 17
+try again
+enter r to roll the dicer
+dice value is  4
+score is 21
+try again
+enter r to roll the dicer
+dice value is  6
+score is 27
+try again
+enter r to roll the dicer
+dice value is  4
+score is 31
+try again
+enter r to roll the dicer
+dice value is  5
+score is 36
+try again
+enter r to roll the dicer
+dice value is  1
+score is 37
+try again
+enter r to roll the dicer
+dice value is  2
+score is 39
+try again
+enter r to roll the dicer
+dice value is  5
+score is 44
+try again
+enter r to roll the dicer
+dice value is  1
+score is 45
+try again
+enter r to roll the dicer
+dice value is  5
+score is 50
+try again
+enter r to roll the dicer
+dice value is  6
+score is 56
+try again
+enter r to roll the dicer
+dice value is  5
+score is 61
+try again
+enter r to roll the dicer
+dice value is  2
+score is 63
+try again
+enter r to roll the dicer
+dice value is  2
+score is 65
+snake bite
+enter r to roll the dicer
+dice value is  2
+score is 48
+try again
+enter r to roll the dicer
+dice value is  6
+score is 54
+try again
+enter r to roll the dicer
+dice value is  6
+score is 60
+try again
+enter r to roll the dicer
+dice value is  6
+score is 66
+try again
+enter r to roll the dicer
+dice value is  5
+score is 71
+try again
+enter r to roll the dicer
+dice value is  2
+score is 73
+try again
+enter r to roll the dicer
+dice value is  4
+score is 77
+try again
+enter r to roll the dicer
+dice value is  1
+score is 78
+try again
+enter r to roll the dicer
+dice value is  4
+score is 82
+try again
+enter r to roll the dicer
+dice value is  1
+score is 83
+try again
+enter r to roll the dicer
+dice value is  2
+score is 85
+try again
+enter r to roll the dicer
+dice value is  4
+score is 89
+snake bite
+enter r to roll the dicer
+dice value is  2
+score is 72
+try again
+enter r to roll the dicer
+dice value is  6
+score is 78
+try again
+enter r to roll the dicer
+dice value is  5
+score is 83
+try again
+enter r to roll the dicer
+dice value is  2
+score is 85
+try again
+enter r to roll the dicer
+dice value is  1
+score is 86
+try again
+enter r to roll the dicer
+dice value is  4
+score is 90
+try again
+enter r to roll the dicer
+dice value is  4
+score is 94
+try again
+enter r to roll the dicer
+dice value is  5
+score is 99
+try again
+enter r to roll the dicer
+dice value is  1
+score is 100
+winner
+enter r to roll the dicet
+bye
+
