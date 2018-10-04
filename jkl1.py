565e068802a80483d0f887565f360b9de148b4f2
tanushr <43023087+tanushr@users.noreply.github.com>
2018-10-04 10:34:21 +0530

    Add files via upload

--- jkl1.py
+++ jkl1.py
@@ -1,15 +1,19 @@
 welcome to rock paper scissor
-3 = scissor
-enter 'k' for rock,'a' for paper,'s' for scissork
-p wins
-dl205@soetcse:~/tanu123$ python3 jkl.py
-welcome to rock paper scissor
 1 = rock
-enter 'k' for rock,'a' for paper,'s' for scissora
-p wins
+enter 'k' for rock,'a' for paper,'s' for scissors
+c wins
+c score is 3
+player wins
+dl205@soetcse:~/tanu123$ python3 jkl.py
+welcome to rock paper scissor
+2 = paper
+enter 'k' for rock,'a' for paper,'s' for scissork
+c wins
+c score is 1
 dl205@soetcse:~/tanu123$ python3 jkl.py
 welcome to rock paper scissor
 3 = scissor
-enter 'k' for rock,'a' for paper,'s' for scissors
-player wins
+enter 'k' for rock,'a' for paper,'s' for scissora
+c wins
+c score is 2
 
