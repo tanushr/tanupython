f0fcf35767ecf91c09b2d5accafa77d32c3266f4
tanushr <43023087+tanushr@users.noreply.github.com>
2018-11-22 10:34:11 +0530

    Add files via upload

--- handcricket.py
+++ handcricket.py
@@ -1,25 +1,60 @@
 import random
 a=int(input("enter value for the toss "))
-if(a%2==0):
+u=random.randint(1,10)
+print(u)
+p=True
+
+if((a+u)%2==0):
 	print("player start playing")
 else:
 	print("computer start playing")
-
+	p = False
+counter=0
 
 computer=0
 score=0
 while True:
-	b=input("enter a character")
-	if(b=='t'):
-		c=int(input("enter value"))
-		score=score+c
-		print(" player score is",score)
-	else:
-		r=random.randint(1,10)
-		computer=computer+r
-		print("computer hits",r)
-		print("computer score is",computer)
+	if counter==2:
+		break
+	if(p):
+		while True:
+			c=int(input("enter value : "))
+			r=random.randint(1,10)
+			score=score+c
+			print(" player score is",score)
+			print("computer hits",r)
+			if(c==r):
+				print("player stops playing")
+				score=score-c
+				p=False
+				counter=counter+1
+				break
 
+			
+	else:
+		while True:
+			r1=random.randint(1,10)
+			a1=int(input("enter value"))
+			computer=computer+r1
+			print("computer hits",r1)
+			print("computer score is",computer)
+			print("player hits",a1)
+			if(r1==a1):
+				print("computer stops playing")
+				computer=computer-r1
+				p=True
+				counter=counter+1
+				break
+
+
+	
+if(score>computer):
+	print("player wins the game",score)
+elif(computer>score):
+	print("computer wins the game",computer)
+else:
+	print("it is a tie")		
+		
 		
 	
 		
