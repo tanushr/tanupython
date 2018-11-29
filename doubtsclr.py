02621cb18d41a31c087a589a89d58c3d687c8edc
tanushr <43023087+tanushr@users.noreply.github.com>
2018-11-29 10:38:42 +0530

    Add files via upload

---
+++ doubtsclr.py
@@ -0,0 +1,13 @@
+dl205@soetcse:~/tanu123$ python3 doubts.py
+5
+dl205@soetcse:~/tanu123$ python3 doubts.py
+5
+dl205@soetcse:~/tanu123$ python3 doubts1.py
+23
+dl205@soetcse:~/tanu123$ python3 doubts2.py
+11
+dl205@soetcse:~/tanu123$ python3 doubts3.py
+ab
+dl205@soetcse:~/tanu123$ python3 doubts4.py
+ab
+
